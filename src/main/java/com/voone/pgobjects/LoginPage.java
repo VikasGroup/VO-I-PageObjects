09f8b106b2e7d029f7871a0ba71de833173f9018
VWIJECH <VWIJECH@VWIJECH-PC>
2016-10-10 14:26:53 +0530

    First commit

---
+++ src/main/java/com/voone/pgobjects/LoginPage.java
@@ -0,0 +1,13 @@
+package com.voone.pgobjects;
+
+import com.titanium.core.BasicPgObject;
+import com.titanium.core.Driver;
+
+public class LoginPage extends BasicPgObject {
+
+	public LoginPage(Driver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
